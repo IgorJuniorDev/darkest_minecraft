35fe752e5b4b2652c8ca79b9a1c1bf5276a92efb
IgorJuniorDev <vishnevskiiigor782@gmail.com>
2025-02-25 19:30:24 +0200

    test 3

--- src/main/java/com/example/structures/ChurchStructure.java
+++ src/main/java/com/example/structures/ChurchStructure.java
@@ -1,18 +1,6 @@
 package com.example.structures;
 
-import com.example.ExampleMod;
 import com.mojang.serialization.Codec;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.structure.StructurePlacementData;
-import net.minecraft.structure.StructureTemplate;
-import net.minecraft.structure.StructureTemplateManager;
-import net.minecraft.util.BlockMirror;
-import net.minecraft.util.BlockRotation;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
